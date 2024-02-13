0f737c5204ec2c6393f3a28d21fe2b7d82427b74
gokhankaya <gokhankaya48@icloud.com>
2024-02-13 23:58:46 +0300

    githubın gitabını

--- src/fatihhh/Gokhan.java
+++ src/fatihhh/Gokhan.java
@@ -4,6 +4,7 @@ public class Gokhan {
     public static void main(String[] args) {
         for (int i = 0; i < 25; i++) {
             System.out.println("sayi = "+i);
+            System.out.println("blaa blaa");
 
         }
     }
