a116e70939c80dff880d7d6c9d1c21000f02b1b8
polat <polat3300@hotmail.com>
2024-02-13 23:43:38 +0300

    eh1

--- src/fatihhh/fatihhhhhhh.java
+++ src/fatihhh/fatihhhhhhh.java
@@ -4,6 +4,9 @@ public class fatihhhhhhh {
     public static void main(String[] args) {
 
         System.out.println("vay siz hoşgelmişşsiniz");
+        for (int i = 0; i < 10; i++) {
+
+        }
 
 
     }
